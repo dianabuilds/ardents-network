e54fd3708c70650ce0ff042a94dbc1ed544cd809
dianabuilds <samiraloveandplay@gmail.com>
2026-08-23 23:59:18 +0300

    refactor(namespace): narrow resolution dependencies

--- internal/naming/resolution/message.go
+++ internal/naming/resolution/message.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 
 	"github.com/dianabuilds/ardents-network/internal/naming"
-	"github.com/dianabuilds/ardents-network/internal/naming/namespace"
+	"github.com/dianabuilds/ardents-network/internal/naming/namespace/admission"
 )
 
 const (
@@ -21,7 +21,7 @@ type resolutionRequest struct {
 	nonce     [32]byte
 	deadline  int64
 	name      string
-	admission namespace.Proof
+	admission admission.Proof
 }
 
 type resolutionResponse struct {
