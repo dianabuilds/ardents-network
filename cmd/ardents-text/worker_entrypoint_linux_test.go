282829d20135d040e30090865f4d9250ed10f38b
dianabuilds <samiraloveandplay@gmail.com>
2026-09-12 10:56:28 +0300

    test: preserve worker environment in entrypoint proof

--- cmd/ardents-text/worker_entrypoint_linux_test.go
+++ cmd/ardents-text/worker_entrypoint_linux_test.go
@@ -57,7 +57,6 @@ func TestWorkerEntrypointAuditsDescriptorsAndJoinsCancellation(t *testing.T) {
 			defer cancel()
 			command := exec.CommandContext(ctx, binaryPath, "worker-publisher")
 			command.Stdin, command.Stdout, command.Stderr = child, child, null
-			command.Env = []string{"GOMAXPROCS=2", "GOMEMLIMIT=96MiB"}
 			if extra {
 				command.ExtraFiles = []*os.File{null}
 			}
